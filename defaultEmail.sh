9b5beb8f0c7c4b34e6ee44a4e5b3d3b17aab07a9
Christopher Kim <chrisjokim@gmail.com>
2018-12-12 14:07:11 -0500

    Added a date and a friendly message to the email.

--- defaultEmail.sh
+++ defaultEmail.sh
@@ -8,6 +8,9 @@ i=1
 
 while IFS='' read -r ADDRESS || [[ -n "$ADDRESS" ]] 
 do
-	echo "This is a test 2" | mutt -e "set crypt_use_gpgme=no" -a "/home/pi/Documents/RedditScraper/testChart.csv" -s "For my lifelines" -- $ADDRESS
+	echo "This is your scheduled news letter for $(date +%A," "%B" "%d)
+	
+	"Who's to say today won't be the best day of your life?"
+	"| mutt -e "set crypt_use_gpgme=no" -a "/home/pi/Documents/RedditScraper/testChart.csv" -s "For my lifelines" -- $ADDRESS
 
 done < "$EMAIL_LIST" 
